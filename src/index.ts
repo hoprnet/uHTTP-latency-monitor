47c59fb6087283b3c5784f8e4aed6531a0595dbc
ausias-armesto <ausiasarmesto@gmail.com>
2024-09-10 11:35:04 +0200

    Fix metrics instantiation and labels

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ type Settings = {
     pushGateway?: string;
     intervalMs: number;
     offsetMs: number;
+    metricLabels: { [key: string]: string };
+    metrics: {[key:string]: prom.Summary};
 };
 
 // if this file is the entrypoint of the nodejs process
@@ -57,15 +59,86 @@ if (require.main === module) {
         forceZeroHop,
         rpcProvider,
     };
-    const settings = {
+    const settings: Settings = {
         pushGateway,
         intervalMs,
         offsetMs,
+        metrics: {},
+        metricLabels: {
+            hops: forceZeroHop ? '0' : '1',
+        },
     };
     const logOpts = {
         uHTTPsettings,
         settings,
     };
+
+    if (process.env.UHTTP_LM_METRIC_INSTANCE !== undefined) {
+        settings.metricLabels['instance'] = process.env.UHTTP_LM_METRIC_INSTANCE;
+    }
+
+    if (process.env.UHTTP_LM_METRIC_REGION !== undefined) {
+        settings.metricLabels['region'] = process.env.UHTTP_LM_METRIC_REGION;
+    }
+
+    if (process.env.UHTTP_LM_METRIC_ZONE !== undefined) {
+        settings.metricLabels['zone'] = process.env.UHTTP_LM_METRIC_ZONE;
+    }
+
+    if (process.env.UHTTP_LM_METRIC_LOCATION !== undefined) {
+        settings.metricLabels['location'] = process.env.UHTTP_LM_METRIC_LOCATION;
+    }
+
+    if (process.env.UHTTP_LM_METRIC_LATITUDE !== undefined) {
+        settings.metricLabels['latitude'] = process.env.UHTTP_LM_METRIC_LATITUDE;
+    }
+
+    if (process.env.UHTTP_LM_METRIC_LONGITUDE !== undefined) {
+        settings.metricLabels['longitude'] = process.env.UHTTP_LM_METRIC_LONGITUDE;
+    }
+
+    const labelNames = Object.keys(settings.metricLabels);
+    settings.metrics['errorSum'] = new prom.Summary({
+        name: `uhttp_error`,
+        help: 'Latency measure not possible due to error',
+        labelNames,
+    });
+
+    settings.metrics['fetchSum'] = new prom.Summary({
+        name: `uhttp_latency_milliseconds`,
+        help: 'Total latency of successful request',
+        labelNames,
+        percentiles: [0.5, 0.7, 0.9, 0.99],
+    });
+
+    settings.metrics['rpcSum'] = new prom.Summary({
+        name: `uhttp_rpc_call_milliseconds`,
+        help: 'The total duration of a round-trip RPC call',
+        labelNames,
+        percentiles: [0.5, 0.7, 0.9, 0.99],
+    });
+
+    settings.metrics['exitAppSum'] = new prom.Summary({
+        name: `uhttp_exit_app_milliseconds`,
+        help: 'Approximate total execution time spent in the exit application, excluding RPC call duration',
+        labelNames,
+        percentiles: [0.5, 0.7, 0.9, 0.99],
+    });
+
+    settings.metrics['segSum'] = new prom.Summary({
+        name: `uhttp_segment_sending_milliseconds`,
+        help: 'Total duration of sending all segments to the hoprd entry node, including acknowledgment receipt',
+        labelNames,
+        percentiles: [0.5, 0.7, 0.9, 0.99],
+    });
+
+    settings.metrics['hoprSum'] =new prom.Summary({
+        name: `uhttp_hopr_network_milliseconds`,
+        help: 'Estimated duration through the HOPR mixnet back and forth',
+        labelNames,
+        percentiles: [0.5, 0.7, 0.9, 0.99],
+    });
+
     log.info('Latency Monitor[%s] started with %o', Version, logOpts);
 
     start(uHTTPsettings, settings);
@@ -88,60 +161,25 @@ function tick(uClient: Routing.Client, uHTTPsettings: UHTTPsettings, settings: S
     const hops = uHTTPsettings.forceZeroHop ? 0 : 1;
     runner
         .once(uClient, uHTTPsettings.rpcProvider)
-        .then(collectMetrics(hops))
-        .catch(reportError(hops))
+        .then(collectMetrics(settings.metrics, settings.metricLabels))
+        .catch(reportError(settings.metrics, settings.metricLabels))
         .finally(pushMetrics(settings.pushGateway));
 }
 
-function collectMetrics(hops: number) {
-    return function (metrics: runner.Durations) {
-        const fetchSum = new prom.Summary({
-            name: `uhttp_latency_milliseconds`,
-            help: 'Total latency of successful request',
-            labelNames: ['hops', 'location'] as const,
-            percentiles: [0.5, 0.7, 0.9, 0.99],
-        });
-        const rpcSum = new prom.Summary({
-            name: `uhttp_rpc_call_milliseconds`,
-            help: 'The total duration of a round-trip RPC call',
-            labelNames: ['hops', 'location'] as const,
-            percentiles: [0.5, 0.7, 0.9, 0.99],
-        });
-        const exitAppSum = new prom.Summary({
-            name: `uhttp_exit_app_milliseconds`,
-            help: 'Approximate total execution time spent in the exit application, excluding RPC call duration',
-            labelNames: ['hops', 'location'] as const,
-            percentiles: [0.5, 0.7, 0.9, 0.99],
-        });
-        const segSum = new prom.Summary({
-            name: `uhttp_segment_sending_milliseconds`,
-            help: 'Total duration of sending all segments to the hoprd entry node, including acknowledgment receipt',
-            labelNames: ['hops', 'location'] as const,
-            percentiles: [0.5, 0.7, 0.9, 0.99],
-        });
-        const hoprSum = new prom.Summary({
-            name: `uhttp_hopr_network_milliseconds`,
-            help: 'Estimated duration through the HOPR mixnet back and forth',
-            labelNames: ['hops', 'location'] as const,
-            percentiles: [0.5, 0.7, 0.9, 0.99],
-        });
-        fetchSum.observe({ hops }, metrics.fetchDur);
-        rpcSum.observe({ hops }, metrics.rpcDur);
-        exitAppSum.observe({ hops }, metrics.exitAppDur);
-        segSum.observe({ hops }, metrics.segDur);
-        hoprSum.observe({ hops }, metrics.hoprDur);
+function collectMetrics(metrics: { [key: string]: prom.Summary }, metricLabels: { [key: string]: string }) {
+    return function (metricsDurations: runner.Durations) {
+        metrics['fetchSum'].observe(metricLabels, metricsDurations.fetchDur);
+        metrics['rpcSum'].observe(metricLabels, metricsDurations.rpcDur);
+        metrics['exitAppSum'].observe(metricLabels, metricsDurations.exitAppDur);
+        metrics['segSum'].observe(metricLabels, metricsDurations.segDur);
+        metrics['hoprSum'].observe(metricLabels, metricsDurations.hoprDur);
     };
 }
 
-function reportError(hops: number) {
+function reportError(metrics: { [key: string]: prom.Summary }, metricLabels: { [key: string]: string }) {
     return function (err: Error) {
         log.error('Error trying to check latency: %s', err);
-        const errorSum = new prom.Summary({
-            name: `uhttp_error`,
-            help: 'Latency measure not possible due to error',
-            labelNames: ['hops', 'location'] as const,
-        });
-        errorSum.observe({ hops }, 0);
+        metrics['errorSum'].observe(metricLabels, 0);
     };
 }
 
